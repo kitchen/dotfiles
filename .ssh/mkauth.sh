52bb4b5d31de25ac3217f8f43c4e41c5ee0f848a
Jeremy Kitchen <kitchen@kitchen.io>
2012-06-30 18:56:21 -0700

    formatting change for authorized_keys file

--- .ssh/mkauth.sh
+++ .ssh/mkauth.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "# --- Kitchen's authorized_keys file generated `date`" >> authorized_keys.tmp
+echo "# Kitchen's authorized_keys file generated `date`" >> authorized_keys.tmp
 for i in pubkeys/*.pub 
 do
-	echo "# - $i" >> authorized_keys.tmp
+	echo "## $i" >> authorized_keys.tmp
 	cat $i >> authorized_keys.tmp
 	echo >> authorized_keys.tmp
 done
