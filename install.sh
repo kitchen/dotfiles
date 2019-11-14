2b4b99c20a4a22dca571ec33f2ed072120549400
Jeremy Kitchen <kitchen@kitchen.io>
2019-11-14 09:24:13 -0800

    stow is *really* smart.
    It will do something something unfolding or something.
    
    i.e. one stow can have:
    .config/foo/bar.inc
    
    another can have:
    .config/bar/baz.inc
    
    and stow will create directories and symlink as appropriate so they
    don't smash each other. this is freaking awesome. I love it.
    
    Also you can pass multiple stows. So that's a thing.
    
    Where has this been all my life :)

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-dirs=(
-	~/.config
-	~/.ssh
-)
-
 stows=(
 	bash
 	git
@@ -22,24 +17,4 @@ if ! which stow > /dev/null; then
 	exit 2
 fi
 
-for dir in ${dirs[@]}; do
-	echo "checking on $dir"
-	if [[ ! -d $dir ]]; then
-		if [[ -e $dir ]]; then
-			echo "$dir exists and is not a directory! eek!"
-			exit 2
-		fi
-
-		echo "creating $dir"
-		mkdir $dir
-	fi
-done
-
-for stow in ${stows[@]}; do
-	echo "stowing $stow"
-	stow $stow
-	if [[ $? -ne 0 ]]; then
-		echo "stow ${stow} exited non-zero, bailing"
-		exit 2
-	fi
-done
+stow ${stows[@]}
