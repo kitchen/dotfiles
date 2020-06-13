78ea28fc3615968c44bdd8de77a69d8d7f2abb72
Jeremy Kitchen <kitchen@kitchen.io>
2020-06-13 14:38:46 -0700

    couple fixes to signmebro
    previously, no matter the input file, the signed certificate was getting
    written out to the ed25519-cert.pub file. oops. This better supports
    non-ed25519 files. I want to continue this and have it have a search
    pattern or something that it uses so I can support other certificate
    types in the future (e.g. ecdsa-sk, since my yubikey neo doesn't support
    ed25519-sk, it seems) but for now this is fine. The only device I ssh
    into for non-work things at the moment doesn't support -sk options, so
    it's moot :)
    
    Also, I made `vault login` stop dumping my token to the terminal when I
    use it. This should make it safer to make a demo screencast or something
    if I want to.

--- ssh/.config/zsh/ssh-ca.zsh
+++ ssh/.config/zsh/ssh-ca.zsh
@@ -3,9 +3,10 @@ function signmebro() {
 		export VAULT_ADDR="http://localhost:8200/"
 		pubkeyfile=${1:-${HOME}/.ssh/id_ed25519.pub}
 		certfile=$(dirname ${pubkeyfile})/$(basename ${pubkeyfile} .pub)-cert.pub
-		vault login -method=userpass username=kitchen
+		echo "signing ${pubkeyfile} and installing it to ${certfile}"
+		vault login -method=userpass -no-print username=kitchen
 		cert=$(vault write -field signed_key sshca/sign/kitchen public_key=@${pubkeyfile})
 		echo cert is: $cert
-		echo $cert > ~/.ssh/id_ed25519-cert.pub
+		echo $cert > $certfile
 	)
 }
