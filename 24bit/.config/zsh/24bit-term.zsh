d7f9dc173c2f5deff63b7894ac27f39e71541136
Jeremy Kitchen <kitchen@kitchen.io>
2020-06-14 23:40:22 -0700

    root doesn't know about 24bit, stop the warnings

--- 24bit/.config/zsh/24bit-term.zsh
+++ 24bit/.config/zsh/24bit-term.zsh
@@ -3,3 +3,10 @@
 if [[ $TERM == "xterm-256color" ]]; then
     export TERM=xterm-24bit
 fi
+
+if [[ $TERM =~ "24bit" ]]; then
+    # because root's terminfo knows bupkis about 24 bit terminals, of course
+    alias sudo="env TERM=$(basename $TERM -24bit)-256color sudo"
+    # probably gonna have to do something like this with ssh, but not really sure how I'm going to go about doing that ...
+fi
+
