6280d59626c917085bb51a5e9d15bd46a1999770
Jeremy Kitchen <kitchen@kitchen.io>
2020-10-05 21:54:19 -0700

    upgraded doom

---
+++ doom-emacs/.doom.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((org-roam-directory . "~/Documents/org/grAy-Team/")
+     (org-log-into-drawer t)
+     (org-roam-directory . "~/Documents/org/roam/")
+     (org-roam-directory . "~/Documents/org/tiamatters/")
+     (org-roam-directory . "~/Documents/org/avernus/"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
