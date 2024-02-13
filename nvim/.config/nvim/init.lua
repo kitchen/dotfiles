570f047b23dbdd426c08d8bcf1d5ef2464c5eb9f
Jeremy Kitchen <kitchen@kitchen.io>
2024-02-13 18:07:39 +0000

    add some zmk extensions to be dts (devicetree) files
    I would love to figure out if there's a way to do this on a per project
    basis similar to how helix did it and similar to editorconfig.
    
    I know editorconfig is a thing but it doesn't have the capability of
    setting an editor filetype as far as I can tell.
    
    I don't like having these be globally overridden, in case there are
    conflicts between projects. Also I'm not entirely certain this is the
    correct way to do this without stomping all over the existing devicetree
    definition (with helix I had to basically copy the entire upstream
    definition over just to add some file extensions, oof)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- would really like to localize this to a project folder
 vim.filetype.add({
   extension = {
     keymap = "dts",
+    overlay = "dts",
   },
 })
